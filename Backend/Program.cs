167ef9f1534864379765aa1e3d73a76983f19f7c
PA <programm-admin@users.noreply.github.com>
2025-09-09 16:21:08 +0200

    UPDATE program with db provider

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,12 +1,32 @@
+using Backend.Data;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("DB connection was not provided in app.");
+}
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDBProvider>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDBProvider>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
