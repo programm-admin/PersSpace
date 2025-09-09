60b8b9c0d3d2a5f76b125eb62b6231e37219d89c
PA <programm-admin@users.noreply.github.com>
2025-09-09 16:17:02 +0200

    CREATE models and db context

---
+++ Backend/Data/AppDBProvider.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Backend.Models;
+
+namespace Backend.Data
+{
+    public class AppDBProvider: DbContext
+    {
+        public AppDBProvider(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<M_User> Users { get; set; }
+        public DbSet<M_Event> Events { get; set; }
+        public DbSet<M_LoginHistory> LoginHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<M_Event>()
+                .HasOne(e => e.User)
+                .WithMany(e => e.Events)
+                .HasForeignKey(e => e.UserAccountID);
+
+            // seed data
+            modelBuilder.Entity<M_User>().HasData(
+                new M_User { ID = "U-1", Name = "Erster Nutzer", Password = "Nutzerpassword" }
+            );
+
+            modelBuilder.Entity<M_Event>().HasData(
+                new M_Event
+                {
+                    ID = "E-1",
+                    Title = "erstes Event",
+                    UserAccountID = "U-1",
+                    Notes = "erste Notizen",
+                    Start = new DateTime(2025, 9, 9),
+                    End = new DateTime(2025, 9, 20),
+                    Created = new DateTime(),
+                    IsDone = false
+                },
+                new M_Event
+                {
+                    ID = "E-2",
+                    Title = "2. Event",
+                    UserAccountID = "U-1",
+                    Notes = "Weitere Notizen zu diesem Event",
+                    Start = new DateTime(2025, 9, 9),
+                    End = new DateTime(2025, 9, 20),
+                    Created = new DateTime(),
+                    IsDone = false
+                }
+            );
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Backend/DataTransferObjects/M_EventDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.DataTransferObjects
+{
+    public class M_EventDTO
+    {
+        public string ID { get; set; }
+        public string UserAccountID { get; set; }
+        public string Title { get; set; }
+        public string Notes { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
