0b6d7932e2c6cdb05571540c2051278a1301c9b1
PA <programm-admin@users.noreply.github.com>
2025-10-02 20:52:45 +0200

    UPDATE CSS; IMPLEMENT loading screen

---
+++ Frontend/src/app/_components/layout/comp-loading-screen/comp-loading-screen.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompLoadingScreen } from './comp-loading-screen';
+
+describe('CompLoadingScreen', () => {
+  let component: CompLoadingScreen;
+  let fixture: ComponentFixture<CompLoadingScreen>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CompLoadingScreen]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CompLoadingScreen);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
