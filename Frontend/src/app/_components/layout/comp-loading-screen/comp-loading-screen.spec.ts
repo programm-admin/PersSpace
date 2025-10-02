5c1783ee8be38ec5a9527178db5d02c049b6d016
PA <programm-admin@users.noreply.github.com>
2025-10-02 20:56:15 +0200

    FORMAT files

--- Frontend/src/app/_components/layout/comp-loading-screen/comp-loading-screen.spec.ts
+++ Frontend/src/app/_components/layout/comp-loading-screen/comp-loading-screen.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompLoadingScreen } from './comp-loading-screen';
 
 describe('CompLoadingScreen', () => {
-  let component: CompLoadingScreen;
-  let fixture: ComponentFixture<CompLoadingScreen>;
+    let component: CompLoadingScreen;
+    let fixture: ComponentFixture<CompLoadingScreen>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CompLoadingScreen]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CompLoadingScreen],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CompLoadingScreen);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CompLoadingScreen);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
