ded72904dbc4a189cc3f5c25469c3de4cbec3e48
PA <programm-admin@users.noreply.github.com>
2025-12-24 14:18:03 +0100

    [16] add new migration

---
+++ Backend/Migrations/20251224131747_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PictureUrl = table.Column<string>(type: "text", nullable: false),
+                    GoogleID = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoginHistories",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    UserAccountID = table.Column<string>(type: "text", nullable: false),
+                    Login = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    M_UserID = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginHistories", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_LoginHistories_Users_M_UserID",
+                        column: x => x.M_UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MediaEvents",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserAccountID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Notes = table.Column<string>(type: "text", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsDone = table.Column<bool>(type: "boolean", nullable: false),
+                    MediaEventCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaEvents", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MediaEvents_Users_UserAccountID",
+                        column: x => x.UserAccountID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoginHistories_M_UserID",
+                table: "LoginHistories",
+                column: "M_UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MediaEvents_UserAccountID",
+                table: "MediaEvents",
+                column: "UserAccountID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoginHistories");
+
+            migrationBuilder.DropTable(
+                name: "MediaEvents");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
