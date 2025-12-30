740c3911948eea65cf02bb195a6b6819eadec7de
PA <programm-admin@users.noreply.github.com>
2025-12-31 00:34:39 +0100

    [3] fix import bugs caused by new file structure

--- Frontend/src/app/presentation/guards/auth-guard/auth-guard.ts
+++ Frontend/src/app/presentation/guards/auth-guard/auth-guard.ts
@@ -1,8 +1,8 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { UC_User_GetIsUserLoggedIn } from '../../core/use-cases/user/get-is-user-logged-in.use-case';
-import { UC_String_GetPathFromRoute } from '../../core/use-cases/string/get-path-from-route.use-case';
-import { APPLICATION_ROUTES } from '../../shared/variables/application-routes';
+import { UC_User_GetIsUserLoggedIn } from '../../../core/use-cases/user/get-is-user-logged-in.use-case';
+import { UC_String_GetPathFromRoute } from '../../../core/use-cases/string/get-path-from-route.use-case';
+import { APPLICATION_ROUTES } from '../../../shared/variables/application-routes';
 
 export const authGuard: CanActivateFn = (route, state) => {
     const getIsLoggedInUseCase = inject(UC_User_GetIsUserLoggedIn);
