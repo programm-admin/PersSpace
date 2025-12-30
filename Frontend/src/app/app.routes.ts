ae74074bfce57599c12d27a44d15ae65e760cc2c
PA <programm-admin@users.noreply.github.com>
2025-12-30 17:52:02 +0100

    [23] fix bug with empty routes list in frontend

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { APPLICATION_ROUTES } from './shared/variables/application-routes';
 
 export const routes: Routes = [
-    APPLICATION_ROUTES.initial,
-    APPLICATION_ROUTES.start,
-    APPLICATION_ROUTES.userStart,
-    APPLICATION_ROUTES.login,
-    APPLICATION_ROUTES.mediaEvent.createEvent,
-    APPLICATION_ROUTES.mediaEvent.showAllEvents,
+    APPLICATION_ROUTES.initial.route,
+    APPLICATION_ROUTES.start.route,
+    APPLICATION_ROUTES.userStart.route,
+    APPLICATION_ROUTES.login.route,
+    APPLICATION_ROUTES.mediaEvent.createEvent.route,
+    APPLICATION_ROUTES.mediaEvent.showAllEvents.route,
 ];
