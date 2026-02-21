dd592f81c5f2ca966323427f9e5d3597de159191
PA <programm-admin@users.noreply.github.com>
2026-02-21 13:36:41 +0100

    [8] update update function with better db method

--- Backend/infrastructure/persitence/MediaEvent.Repository.cs
+++ Backend/infrastructure/persitence/MediaEvent.Repository.cs
@@ -3,66 +3,91 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Entities;
 using Infrastructure.Mappers;
+using Application.Exceptions;
 
 public class MediaEventRepository(AppDBProvider db) : IMediaEventRepository
 {
-
-    public async Task AddMediaEvent(MediaEvent mediaEvent)
-    {
-        MediaEventEntity mediaEventEntity = new MediaEventEntity
-        {
-            Id = mediaEvent.ID,
-            UserAccountId = mediaEvent.UserAccountID,
-            Title = mediaEvent.Title,
-            Notes = mediaEvent.Notes,
-            Start = mediaEvent.Start,
-            End = mediaEvent.End,
-            IsDone = mediaEvent.IsDone,
-            MediaEventCreated = mediaEvent.MediaEventCreated
-        };
-
-        db.MediaEvents.Add(mediaEventEntity);
-        await db.SaveChangesAsync();
-    }
-
-    public async Task DeleteMediaEvent(Guid userId, Guid eventId)
-    {
-        MediaEventEntity? entity = await db.MediaEvents.FirstOrDefaultAsync(ev => ev.Id == eventId && ev.UserAccountId == userId);
-
-        if (entity is null) return;
-
-        db.MediaEvents.Remove(entity);
-        await db.SaveChangesAsync();
-    }
-
     public async Task<IReadOnlyList<MediaEvent>> GetAllMediaEventsForUser(Guid userAccountId)
     {
-        List<MediaEventEntity> entities = await db.MediaEvents.AsNoTracking().Where(ev => ev.UserAccountId == userAccountId).OrderBy(ev => ev.Title).ToListAsync();
+        try
+        {
+            List<MediaEventEntity> entities = await db.MediaEvents.AsNoTracking().Where(ev => ev.UserAccountId == userAccountId).OrderBy(ev => ev.Title).ToListAsync();
 
-        return [.. entities.Select(MediaEventMapper.ToDomain)];
+            return [.. entities.Select(MediaEventMapper.ToDomain)];
+        }
+        catch (Exception ex)
+        {
+            throw new PersistanceExeption("[ERROR - MediaEventRepository: GetALlMediaEventsForUser()] Error when trying to get media events from DB:", ex);
+        }
     }
 
     public async Task<MediaEvent?> GetMediaEventById(Guid userAccountId, Guid eventId)
     {
-        MediaEventEntity? entity = await db.MediaEvents.AsNoTracking().FirstOrDefaultAsync(e => e.UserAccountId == userAccountId && e.Id == eventId);
+        try
+        {
+            MediaEventEntity? entity = await db.MediaEvents.AsNoTracking().FirstOrDefaultAsync(e => e.UserAccountId == userAccountId && e.Id == eventId);
 
-        if (entity is null) return null;
+            if (entity is null) return null;
 
-        return new MediaEvent(entity.Id, entity.UserAccountId, entity.Title, entity.Notes, entity.Start, entity.End, entity.IsDone, entity.MediaEventCreated);
+            return new MediaEvent(entity.Id, entity.UserAccountId, entity.Title, entity.Notes, entity.Start, entity.End, entity.IsDone, entity.MediaEventCreated);
+        }
+        catch (Exception ex)
+        {
+            throw new PersistanceExeption("[ERROR - MediaEventRepository: GetMediaEventById()] Failed to get media event for given event id and user account id from DB:", ex);
+        }
+    }
+    public async Task AddMediaEvent(MediaEvent mediaEvent)
+    {
+        try
+        {
+            MediaEventEntity mediaEventEntity = MediaEventMapper.ToEntity(mediaEvent);
+
+            db.MediaEvents.Add(mediaEventEntity);
+            await db.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            throw new PersistanceExeption("[ERROR - MediaEventRepository: AddMediaEvent()] Error when trying to insert new media event into DB:", ex);
+        }
     }
 
     public async Task UpdateMediaEvent(MediaEvent mediaEvent)
     {
-        MediaEventEntity? entity = await db.MediaEvents.FirstOrDefaultAsync(ev => ev.Id == mediaEvent.ID & ev.UserAccountId == mediaEvent.UserAccountID);
+        try
+        {
+            int rowsAffectedByUpdate = await db.MediaEvents.Where(ev => ev.Id == mediaEvent.ID && ev.UserAccountId == mediaEvent.UserAccountID).ExecuteUpdateAsync(
+                setters => setters
+                    .SetProperty(ev => ev.Title, mediaEvent.Title)
+                    .SetProperty(ev => ev.Notes, mediaEvent.Notes)
+                    .SetProperty(ev => ev.Start, mediaEvent.Start)
+                    .SetProperty(ev => ev.End, mediaEvent.End)
+                    .SetProperty(ev => ev.IsDone, mediaEvent.IsDone)
+            );
 
-        if (entity is null) throw new InvalidOperationException("[ERROR - MediaEventRepository: UpdateMediaEvent()] No media event found for given event id and user account id.");
+            if (rowsAffectedByUpdate != 1) throw new InvalidOperationException("[ERROR - MediaEventRepository: UpdateMediaEvent()] No media event found for given event id and user account id.");
 
-        entity.Title = mediaEvent.Title;
-        entity.Notes = mediaEvent.Notes;
-        entity.Start = mediaEvent.Start;
-        entity.End = mediaEvent.End;
-        entity.IsDone = mediaEvent.IsDone;
+            await db.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            throw new PersistanceExeption("[ERROR - MediaEventRepository: UpdateMediaEvent()] Error when trying to update media event:", ex);
+        }
+    }
 
-        await db.SaveChangesAsync();
+    public async Task DeleteMediaEvent(Guid userId, Guid eventId)
+    {
+        try
+        {
+            MediaEventEntity? entity = await db.MediaEvents.FirstOrDefaultAsync(ev => ev.Id == eventId && ev.UserAccountId == userId);
+
+            if (entity is null) return;
+
+            db.MediaEvents.Remove(entity);
+            await db.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex)
+        {
+            throw new PersistanceExeption("[ERROR - MediaEventRepository: UpdateMediaEvent()] Error when trying to delete media event:", ex);
+        }
     }
 }
